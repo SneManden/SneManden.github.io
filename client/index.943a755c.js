fa16dbb86d3014b450d348e4a8c7fa5d39a3ea46
Casper Kehlet Jensen <casper@snemanden.com>
2021-07-17 19:46:51 +0200

    Add personal blog

---
+++ client/index.943a755c.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e,o,g as r}from"./client.cb032e34.js";function a(s){return o(()=>r("/blog")),[]}export default class extends s{constructor(s){super(),t(this,s,a,null,e,{})}}
